d77466c21b5f62819e1e5d6e9c547efdf5cc043f
codevory <shahijahan.codes@gmail.com>
2026-05-06 16:30:18 +0530

    controllers for user routes

---
+++ Express/restApi/controllers/getAllData.js
@@ -0,0 +1,33 @@
+import { startups } from "../data.js";
+
+export const getAllData = (req, res) => {
+  let filteredData = startups;
+  const { industry, country, continent, is_seeking_funding, has_mvp } =
+    req.query;
+  if (industry) {
+    filteredData = filteredData.filter(
+      (d) => d.industry.toLowerCase() === industry.toLowerCase(),
+    );
+  }
+  if (country) {
+    filteredData = filteredData.filter(
+      (d) => d.country.toLowerCase() === country.toLowerCase(),
+    );
+  }
+  if (continent) {
+    filteredData = filteredData.filter(
+      (d) => d.continent.toLowerCase() === continent.toLowerCase(),
+    );
+  }
+  if (is_seeking_funding) {
+    filteredData = filteredData.filter(
+      (d) => d.is_seeking_funding === Boolean(is_seeking_funding.toLowerCase()),
+    );
+  }
+  if (has_mvp) {
+    filteredData = filteredData.filter(
+      (d) => d.has_mvp === Boolean(has_mvp.toLowerCase()),
+    );
+  }
+  res.json(filteredData);
+};
---
+++ Express/restApi/controllers/getDataByPathParams.js
@@ -0,0 +1,23 @@
+import { startups } from "../data.js";
+
+export const getDataByPathParams = (req, res) => {
+  let filteredData = startups;
+
+  console.log(req.params);
+  const { field, term } = req.params;
+  const errorMessage = {
+    message:
+      "Search field not allowed. Please use only 'country', 'continent', 'industry'",
+  };
+  const allowedFields = ["country", "continent", "industry"];
+
+  if (!allowedFields.includes(field)) {
+    return res.status(400).json(errorMessage);
+  }
+  if (field && term) {
+    filteredData = filteredData.filter(
+      (d) => d[field].toLowerCase() === term.toLowerCase(),
+    );
+  }
+  res.json(filteredData);
+};
