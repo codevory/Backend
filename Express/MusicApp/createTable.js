f43283eb7b93607114c4699a2a597de135479449
codevory <shahijahan.codes@gmail.com>
2026-05-07 13:42:13 +0530

    async function that creates tables in database

---
+++ Express/MusicApp/createTable.js
@@ -0,0 +1,27 @@
+import path from "node:path";
+import { open } from "sqlite";
+import sqlite3 from "sqlite3";
+
+async function createTable() {
+  const db = await open({
+    filename: path.join("database.db"),
+    driver: sqlite3.Database,
+  });
+
+  await db.exec(
+    `CREATE TABLE IF NOT EXISTS products(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        title TEXT NOT NULL,
+        price REAL NOT NULL,
+        image TEXT NOT NULL,
+        year INTEGER,
+        genre TEXT,
+        artist TEXT,
+        stock INTEGER
+        )`,
+  );
+
+  db.close(() => console.log("database table created"));
+}
+
+createTable();
