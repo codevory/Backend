bc5ba30fbb9308c9297ba588046f3043dbe690a9
codevory <shahijahan.codes@gmail.com>
2026-05-09 21:50:32 +0530

    database connection establisher function

--- Express/MusicApp/db/db.js
+++ Express/MusicApp/db/db.js
@@ -2,8 +2,8 @@ import sqlite3 from "sqlite3";
 import { open } from "sqlite";
 import path from "node:path";
 
-export async function getDBConnection() {
-  const dbPath = path.join("database.db");
+export async function getDBConnection(fileName) {
+  const dbPath = path.join(fileName || "database.db");
 
   return open({
     filename: dbPath,
