930a9684b74d9cdad10a680a1545019507c683ed
codevory <shahijahan.codes@gmail.com>
2026-05-09 21:54:44 +0530

    server configurations

--- Express/MusicApp/server.js
+++ Express/MusicApp/server.js
@@ -1,17 +1,41 @@
 import express from "express";
-import cors from "cors";
 import { productRouter } from "./routes/products.js";
+import { authRouter } from "./routes/auth.js";
+import cors from "cors";
+import session from "express-session";
+import dotenv from "dotenv";
+import { meRouter } from "./routes/me.js";
+import { cartRouter } from "./routes/cart.js";
 
+dotenv.config();
+const Secret = process.env.SPIRAL_SESSION_SECRET;
 const app = express();
+const PORT = 2820;
 
 app.use(cors());
+app.use(express.json());
+app.use(
+  session({
+    secret: Secret,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      httpOnly: true,
+      secure: false,
+      sameSite: "lax",
+    },
+  }),
+);
 app.use(express.static("public"));
 app.use("/api/products", productRouter);
+app.use("/api/auth/me", meRouter);
+app.use("/api/auth", authRouter);
+app.use("/api/cart", cartRouter);
 
 app
-  .listen(2820, () => {
-    console.log("server running at : http://localhost:2820");
+  .listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
   })
   .on("error", (err) => {
-    console.log("Failed to start server ", err.message);
+    console.error("Failed to start server:", err);
   });
