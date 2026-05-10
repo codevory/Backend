b56a0dc01d6336a3efe3d5fe289e07adc6355abb
codevory <shahijahan.codes@gmail.com>
2026-05-10 21:53:06 +0530

    implement delete Item & checkout functionality to cart

--- Express/MusicApp/controllers/cartController.js
+++ Express/MusicApp/controllers/cartController.js
@@ -55,47 +55,11 @@ export async function getCartCount(req, res) {
 }
 
 export async function getAll(req, res) {
-  // Don't touch this code!
   if (!req.session.userId) {
     return res.json({ err: "not logged in" });
   }
 
   const db = await getDBConnection();
-
-  /*
-Challenge: 
-
-1. When a logged-in user clicks the cart icon, they will be redirected to the cart.html page. To render the user's cart, the frontend needs to get an array of objects similar to the example below when it makes a GET request to the /api/cart endpoint. Important: this array should be sent in a JSON object with the key 'items'.
-
-[
-  {
-    cartItemId: 4,
-    quantity: 2,
-    title: 'Selling Dogma',
-    artist: 'The Clouds',
-    price: 44.99
-  },
-  {
-    cartItemId: 5,
-    quantity: 1,
-    title: 'Midnight Parallels',
-    artist: 'Neon Grove',
-    price: 40.99
-  }
-]
-
-The frontend JS has been done for you.
-
-Ignore frontend console errors for now!
- 
-For testing, log in with:
-Username: test
-Password: test
-
-Then click the cart icon to go to the cart page. You should see the user’s items.
-
-Loads of help in hint.md
-*/
   try {
     const items = await db.all(
       `SELECT ci.id AS cartItemId, ci.quantity,p.title,p.artist,p.price FROM cart_items 
@@ -106,5 +70,56 @@ Loads of help in hint.md
     res.status(200).json({ items: items });
   } catch (err) {
     console.error("Error getting cart Items : ", err.message);
+  }
+}
+
+export async function deleteItem(req, res) {
+  const db = await getDBConnection();
+
+  try {
+    const { itemId } = req.params;
+    const product_id = parseInt(itemId);
+    if (isNaN(product_id)) {
+      return res.status(404).json({ error: "Invalid Product Id" });
+    }
+    const isInCart = await db.get(
+      "SELECT * FROM cart_items WHERE user_id = ? AND id = ?",
+      [req.session.userId, product_id],
+    );
+
+    if (!isInCart) {
+      return res.status(404).json({ error: "Invalid Item ID" });
+    }
+
+    await db.run("DELETE FROM cart_items WHERE user_id = ? AND id = ?", [
+      req.session.userId,
+      product_id,
+    ]);
+    console.log("Item with id : ", product_id, " deleted successfully");
+    res.status(204).send();
+  } catch (err) {
+    console.error("Failed to delete Item : ", err.message);
+  }
+}
+
+export async function deleteAll(req, res) {
+  const db = await getDBConnection();
+
+  try {
+    const isValidCart = await db.run(
+      "SELECT * FROM cart_items WHERE user_id = ?",
+      [req.session.userId],
+    );
+    if (!isValidCart) {
+      return res.status(404).json({ error: "No cart Items exist" });
+    }
+    await db.run("DELETE FROM cart_items WHERE user_id = ? ", [
+      req.session.userId,
+    ]);
+    res.status(204).send();
+    console.log("order placed successfully");
+  } catch (err) {
+    console.error("Failed to checkout : ", err.message);
+    res.status(400).json({ error: `Failed to process order ${err.message}` });
   }
 }
